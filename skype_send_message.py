7e2ed4f748eada461551551dc6bdbe901357f76d
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2018-09-20 11:17:11 +0300

    Update skype_send_message.py

--- skype_send_message.py
+++ skype_send_message.py
@@ -1,3 +1,4 @@
+import sys
 import keyring
 import argparse
 from skpy import Skype
@@ -5,22 +6,30 @@ parser = argparse.ArgumentParser()
 parent_parser = argparse.ArgumentParser(add_help=False)
 parent_parser.add_argument('--username', action="store")
 parent_parser.add_argument('--password', action="store")
-print("Welcome to 'Skype send message'. ")
-username = input("Type your username of Skype: ")
-password = keyring.get_password("Skype", username) or input("Type your password of Skype: ")
-try:
-    keyring.set_password("Skype", username,  password)
-    print("password stored successfully")
-except keyring.errors.PasswordSetError:
-    print("failed to store password")
-    print("password", keyring.get_password("Skype", username))
-contact = Skype(username, password)
-contact.user  # you
-contact.contacts  # your contacts
-contact.chats  # your conversations
-message = contact.contacts[input("Type user name of Skype for 1-to-1 conversation: ")].chat  # 1-to-1 conversation
-print("Sending: ")
-message.sendMsg(input("Type your message for user of Skype with you want to 1-to-1 conversation: "))
-print("messages: ", message.getMsgs())  # retrieve recent messages
-print("Your message is send")
-#print("password deleted: ", keyring.delete_password("Skype", username))
+parent_parser.add_argument('--servicename', action="store")
+def main(argv):
+    print("Welcome to 'Skype send message'. ")
+    servicename = "Skype"
+    username = input("Type your username of Skype: ")
+    password = keyring.get_password(servicename, username) or input("Type your password of Skype: ")
+    try:
+        keyring.set_password(servicename, username,  password)
+        print("password stored successfully")
+    except keyring.errors.PasswordSetError:
+        print("failed to store password")
+        print("password", keyring.get_password(servicename, username))
+    contact = Skype(username, password)
+    contact.user  # you
+    contact.contacts  # your contacts
+    contact.chats  # your conversations
+    message = contact.contacts[input("Type user name of Skype for 1-to-1 conversation: ")].chat  # 1-to-1 conversation
+    print("Sending: ")
+    message.sendMsg(input("Type your message for user of Skype with you want to 1-to-1 conversation: "))
+    message.getMsgs()
+    print("messages: ", message.getMsgs())  # retrieve recent messages
+    print("Your message is send")
+    print(servicename, username, password)
+    #print("password deleted: ", keyring.delete_password("Skype", username))
+    pass
+if __name__ == '__main__':
+    main(sys.argv)
