25a21ae4cd7c88b236efe1131b41bae87c6e44e2
idumenskyi <42769579+idumenskyi@users.noreply.github.com>
2018-09-22 07:49:27 +0300

    Update skype_send_message.py

--- skype_send_message.py
+++ skype_send_message.py
@@ -6,16 +6,9 @@ parent_parser = argparse.ArgumentParser(add_help=False)
 parent_parser.add_argument('--username', action="store")
 parent_parser.add_argument('--password', action="store")
 parent_parser.add_argument('--servicename', action="store")
-
 def main(servicename, username):
-    print("Welcome to 'Skype send message'. ")
-
-    #servicename = "Skype"
-    #username = input("Type your username of Skype: ")
+    print("Welcome to 'Skype send message'. ")  
     password = keyring.get_password(servicename, username) or input("Type your password of Skype: ")
-    #password = keyring.get_password(servicename, username)
-    #password = input("Type your password of Skype: ")
-    #password = ""
     try:
         keyring.set_password(servicename, username,  password)
         print("password stored successfully")
@@ -33,10 +26,7 @@ def main(servicename, username):
     print("messages: ", message.getMsgs())  # retrieve recent messages
     print("Your message is send")
     print(servicename, username, password)
-    #print("password deleted: ", keyring.delete_password("Skype", username))
-    #return str(servicename), str(username), str(password)
     return str(password)
-
     pass
 if __name__ == '__main__':
     main('servicename', 'username')
